fe03a5fcff1dd2f5dcf349a7f5f03ff0e3fdebf2
RafaPolit <rafaelpolit@gmail.com>
2022-06-10 02:40:25 -0500

    config

---
+++ pages/api/udpSend.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import dgram from "dgram";
+
+type Data = {
+  bytes: number;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const msg = req.query.msg.toString();
+  const client = dgram.createSocket("udp4");
+  client.send(msg + "\n", 7778, "192.168.0.7", (err, bytes) => {
+    if (err) {
+      console.log("err:", err);
+    }
+    res.status(200).json({ bytes });
+  });
+}
