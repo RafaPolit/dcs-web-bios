46cebdb3d99d18d00f0f7456ddc92ba87eec1c25
RafaPolit <rafaelpolit@gmail.com>
2023-01-31 15:59:40 -0500

    Revert "Revert changes"
    This reverts commit 2d9ccaa8ff62b08e2855e8bb7b32adbbe96023bc.

--- socketServer/scripts/relevantData/f-16c.ts
+++ socketServer/scripts/relevantData/f-16c.ts
@@ -1,3 +1,4 @@
+import dcsData from "../dcsBiosDefinitions/F-16C_50.json";
 import { RelevantData } from "../utils";
 
 const relevantData: RelevantData[] = [
@@ -9,44 +10,46 @@ const relevantData: RelevantData[] = [
   },
   {
     identifier: "DED_LINE_1",
-    address: 0x450a,
-    max_length: 29,
+    address: dcsData["DED Output Data"].DED_LINE_1.outputs[0].address,
+    max_length: dcsData["DED Output Data"].DED_LINE_1.outputs[0].max_length,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_2",
-    address: 0x4528,
-    max_length: 29,
+    address: dcsData["DED Output Data"].DED_LINE_2.outputs[0].address,
+    max_length: dcsData["DED Output Data"].DED_LINE_2.outputs[0].max_length,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_3",
-    address: 0x4546,
-    max_length: 29,
+    address: dcsData["DED Output Data"].DED_LINE_3.outputs[0].address,
+    max_length: dcsData["DED Output Data"].DED_LINE_3.outputs[0].max_length,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_4",
-    address: 0x4564,
-    max_length: 29,
+    address: dcsData["DED Output Data"].DED_LINE_4.outputs[0].address,
+    max_length: dcsData["DED Output Data"].DED_LINE_4.outputs[0].max_length,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_5",
-    address: 0x4582,
-    max_length: 29,
+    address: dcsData["DED Output Data"].DED_LINE_5.outputs[0].address,
+    max_length: dcsData["DED Output Data"].DED_LINE_5.outputs[0].max_length,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
 ];
+
+console.log(relevantData);
 
 export { relevantData };
