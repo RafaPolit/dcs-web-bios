2d9ccaa8ff62b08e2855e8bb7b32adbbe96023bc
RafaPolit <rafaelpolit@gmail.com>
2023-01-31 15:50:28 -0500

    Revert changes

--- socketServer/scripts/relevantData/f-16c.ts
+++ socketServer/scripts/relevantData/f-16c.ts
@@ -1,4 +1,3 @@
-import dcsData from "../dcsBiosDefinitions/F-16C_50.json";
 import { RelevantData } from "../utils";
 
 const relevantData: RelevantData[] = [
@@ -10,46 +9,44 @@ const relevantData: RelevantData[] = [
   },
   {
     identifier: "DED_LINE_1",
-    address: dcsData["DED Output Data"].DED_LINE_1.outputs[0].address,
-    max_length: dcsData["DED Output Data"].DED_LINE_1.outputs[0].max_length,
+    address: 0x450a,
+    max_length: 29,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_2",
-    address: dcsData["DED Output Data"].DED_LINE_2.outputs[0].address,
-    max_length: dcsData["DED Output Data"].DED_LINE_2.outputs[0].max_length,
+    address: 0x4528,
+    max_length: 29,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_3",
-    address: dcsData["DED Output Data"].DED_LINE_3.outputs[0].address,
-    max_length: dcsData["DED Output Data"].DED_LINE_3.outputs[0].max_length,
+    address: 0x4546,
+    max_length: 29,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_4",
-    address: dcsData["DED Output Data"].DED_LINE_4.outputs[0].address,
-    max_length: dcsData["DED Output Data"].DED_LINE_4.outputs[0].max_length,
+    address: 0x4564,
+    max_length: 29,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
   {
     identifier: "DED_LINE_5",
-    address: dcsData["DED Output Data"].DED_LINE_5.outputs[0].address,
-    max_length: dcsData["DED Output Data"].DED_LINE_5.outputs[0].max_length,
+    address: 0x4582,
+    max_length: 29,
     string_length: 24,
     control_chars: [25, 26, 27],
     type: "stringAndControl",
   },
 ];
-
-console.log(relevantData);
 
 export { relevantData };
